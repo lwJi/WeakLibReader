14bba6c2b33305d9baa1aad8846f5efba0b09f54
Liwei Ji <jiliwei.phys@gmail.com>
2026-01-27 15:20:38 -0500

    cactus_interface: add parameter eos_table_file

--- cactus_interface/TestWeakLibReader/src/testweaklibreader.cxx
+++ cactus_interface/TestWeakLibReader/src/testweaklibreader.cxx
@@ -3,9 +3,20 @@
 #include <cctk_Parameters.h>
 #include <loop_device.hxx>
 
-#include "WeakLibReader_Hdf5Loader.hpp"
+#include <WeakLibReader_Hdf5Loader.hpp>
 
 namespace TestWeakLibReader {
+
+extern "C" void TestWeakLibReader_LoadTable(CCTK_ARGUMENTS) {
+  DECLARE_CCTK_PARAMETERS;
+
+  CCTK_INFO("Loading EOS table");
+
+  WeakLibReader::Hdf5Table table;
+  const auto status = WeakLibReader::LoadHdf5Table(eos_table_file, table);
+
+  assert(status == WeakLibReader::Hdf5LoadStatus::Success);
+}
 
 extern "C" void TestWeakLibReader_Init(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS;
